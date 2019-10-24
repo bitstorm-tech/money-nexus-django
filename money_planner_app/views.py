97c6c89c066734bbcd935d29a836088beda71331
Josef Bauer <bauerjoe@gmx.de>
2019-10-24 22:23:53 +0200

    save new tags to database

--- money_planner_app/views.py
+++ money_planner_app/views.py
@@ -7,16 +7,18 @@ from money_planner_app.models import Tag
 
 
 def transactions(request: HttpRequest):
-    context = {
-        "now": datetime.date.today().strftime("%Y-%m-%d")
-    }
+    context = {"now": datetime.date.today().strftime("%Y-%m-%d")}
 
     return render(request, "money_planner_app/transactions.html", context)
 
 
 def tags(request: HttpRequest):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        tag = Tag(name=name, description=description)
+        tag.save()
+
     all_tags = Tag.objects.all()
-    context = {
-        "tags": all_tags
-    }
+    context = {"tags": all_tags}
     return render(request, "money_planner_app/tags.html", context)
