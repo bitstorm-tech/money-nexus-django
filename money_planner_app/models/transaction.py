c50bad718f2f97f3cfc1906b6a64e54b2774192c
Josef Bauer <bauerjoe@gmx.de>
2019-11-06 21:34:21 +0100

    Add note field to transaction

--- money_planner_app/models/transaction.py
+++ money_planner_app/models/transaction.py
@@ -5,5 +5,5 @@ class Transaction(models.Model):
     date = models.DateField()
     time = models.TimeField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(null=True)
+    note = models.TextField(null=True)
     tags = models.ManyToManyField("Tag")
