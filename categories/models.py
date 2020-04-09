89224d9d2e1e2330c49852c6bf9a3ff8021e9768
Josef Bauer <bauerjoe@gmx.de>
2020-04-09 21:57:00 +0200

    Fix broken categories

--- categories/models.py
+++ categories/models.py
@@ -5,6 +5,14 @@ class Category(models.Model):
     name = models.TextField()
     description = models.TextField(null=True)
 
+    @staticmethod
+    def from_post_data(post_data) -> "Category":
+        category = Category()
+        category.id = post_data.get("id")
+        category.name = post_data.get("name")
+        category.description = post_data.get("description")
+        return category
+
 
 class SubCategory(models.Model):
     name = models.TextField()
--- categories/views.py
+++ categories/views.py
@@ -8,16 +8,9 @@ def categories(request: HttpRequest):
     context = {"active": "categories"}
 
     if request.method == "POST":
-        save_category(request.POST)
+        Category.from_post_data(request.POST).save()
     else:
         all_categories = list(Category.objects.order_by("name").values())
         context["categories"] = all_categories
 
     return render(request, "categories/categories.html", context)
-
-
-def save_category(post_data):
-    category = Category()
-    category.name = post_data["name"]
-    category.description = post_data["description"]
-    category.save()
